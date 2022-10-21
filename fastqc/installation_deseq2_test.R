81504909bd0b34915251dd3a103e8a77945af2b1
Maïsen Hassani <maisen992@gmail.com>
2022-10-21 11:05:30 +0200

    changement nom dossier

--- fastqc/installation_deseq2_test.R
+++
@@ -1,8 +0,0 @@
-install.packages("DESeq2",dependencies = TRUE)
-
-
-if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-BiocManager::install("DESeq2")
-
-library(DESeq2)
-sessionInfo()
