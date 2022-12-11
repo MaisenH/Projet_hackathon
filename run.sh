a778a2c31d35756cfac1c65e2060f1073c2e686e
MaisenH <96000637+MaisenH@users.noreply.github.com>
2022-12-11 20:23:26 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+## Au préalable, il faut avoir installer nextflow
+
+## Il faut aller dans le répertoire workflow ou sont présents les fichiers suivants:
+  # main.nf et nextflow.config
+ 
+# Executez la commande suivante:
+./nextflow run main.nf
+
